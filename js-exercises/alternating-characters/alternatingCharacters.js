2013b6f16f09a956c3bc81d4154917f37d1c4afa
ankojh <ankitkumarojha2@gmail.com>
2020-09-29 17:32:36 +0530

    Refactor `alternating-characters` solution

--- js-exercises/alternating-characters/alternatingCharacters.js
+++ js-exercises/alternating-characters/alternatingCharacters.js
@@ -22,14 +22,16 @@
 //isEmptyString '' alternating? Yes 0 characters are alternating, number of deletions is obviously zero
 
 
-function getAllSubstringsOfLength(string, length) {
-  const numberOfSubstrings = string.length - length + 1;
+function getAllSubstringsOfLength(string, lengthOfSubstring) {
+  const numberOfSubstrings = string.length - lengthOfSubstring + 1;
   const substrings = new Set();
   for (let i = 0; i < numberOfSubstrings; i++) {
-    substrings.add(string.substr(i, length));
+    substrings.add(string.substr(i, lengthOfSubstring));
   }
   return Array.from(substrings);
 }
+
+
 
 function doesStringHasAllCharactersUnique(string) {
   const charMap = {}
@@ -46,18 +48,15 @@ function doesStringHasAllCharactersUnique(string) {
 
 function getDeletionCountToAlternateStringWithSubstring(string, substring) { //memo it??. nah using set for substring gen.
   let deletionCount = 0;
-  // console.log('\n')
   for (let i = 0; i < string.length; i++) {
     const currentSubstring = string.substr(i, substring.length);
-    // console.log(currentSubstring, substring)
-    if (currentSubstring === substring.substr(0,currentSubstring.length)) {
+    if (currentSubstring === substring.substr(0, currentSubstring.length)) {
       i = i + currentSubstring.length - 1;
     }
     else {
       deletionCount++
     }
   }
-  // console.log(deletionCount)
   return deletionCount;
 }
 
@@ -68,8 +67,10 @@ function getMinDeletionCountToAlternateString(string) {
   const minDeletionCountsForEachLengthSubstring = []
   for (let i = string.length; i > 1; i--) {
     const substrings = getAllSubstringsOfLength(string, i).filter(doesStringHasAllCharactersUnique);
-    if(!substrings.length){
-      minDeletionCountsForEachLengthSubstring.push(string.length -1)
+    
+    //handle single characters alternating
+    if (!substrings.length) {
+      minDeletionCountsForEachLengthSubstring.push(string.length - 1)
       continue;
     }
 
@@ -86,9 +87,6 @@ function getMinDeletionCountToAlternateString(string) {
 function alternatingCharacters(arrOfStrings) {
   return arrOfStrings.map(getMinDeletionCountToAlternateString);
 }
-
-
-// console.log(alternatingCharacters(['AAAA','BBBBBB']));
 
 
 export { alternatingCharacters };
